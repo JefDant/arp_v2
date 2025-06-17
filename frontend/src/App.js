cbda5f696e997b71cbe445632a71108419b2c8c5
JefDant <jjeferson.danta@gmail.com>
2025-06-17 10:53:43 -0300

    Adicionando estrutura básica do frontend React

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ARP - Sistema de Gestão</h1>
+        <p>Bem-vindo ao sistema!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
